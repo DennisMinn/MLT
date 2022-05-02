34e9b0303fcd6606b16cc7622aa2bb2385e76f70
Dennis Minn <minndennis@gmail.com>
2022-05-02 17:12:29 -0400

    changed abstract class forward parameter name to fit computer vision related inputs

--- base/base_model.py
+++ base/base_model.py
@@ -9,7 +9,7 @@ class BaseModel(pl.LightningModule):
     Abstract class for all models
     """
     @abstractmethod
-    def forward(self, input_ids, attention_mask):
+    def forward(self, x):
         """
         Forward pass logic
         :return: Model output
@@ -31,7 +31,7 @@ class BaseModel(pl.LightningModule):
         """
         raise NotImplementedError
 
-     def configure_optimizers(self):
+    def configure_optimizers(self):
         """
         initializes optimizers
         """
